4ba11d65122fa49449347e3578b88a64e51d9b26
将之大西 <omccello@gmail.com>
2021-07-31 21:28:42 +0900

    first

--- ios_tutorial_first_app/ContentView.swift
+++ ios_tutorial_first_app/ContentView.swift
@@ -1,15 +1,8 @@
-//
-//  ContentView.swift
-//  ios_tutorial_first_app
-//
-//  Created by 大西将之 on 2021/07/31.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Swift！")
             .padding()
     }
 }
