7d7b59a9019f77bac808598d8ab99cef178c58da
将之大西 <omccello@gmail.com>
2021-07-31 21:23:15 +0900

    Initial Commit

---
+++ ios_tutorial_first_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_tutorial_first_app
+//
+//  Created by 大西将之 on 2021/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_tutorial_first_app/ios_tutorial_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_tutorial_first_appApp.swift
+//  ios_tutorial_first_app
+//
+//  Created by 大西将之 on 2021/07/31.
+//
+
+import SwiftUI
+
+@main
+struct ios_tutorial_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
