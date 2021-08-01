ddfb1d8a36f4221e141282b99f1f93e4bee24dc5
将之大西 <omccello@gmail.com>
2021-08-01 12:35:12 +0900

    fix

--- ios_tutorial_first_app/ContentView.swift
+++ ios_tutorial_first_app/ContentView.swift
@@ -3,12 +3,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Swift！")
+            .foregroundColor(Color.orange)
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        List {
+            ContentView()
+            ContentView()
+        }
     }
 }
