10de09895a6a2645845293bff9ba4d83a64f5841
将之大西 <omccello@gmail.com>
2021-08-01 12:56:04 +0900

    fix

--- ios_tutorial_first_app/ContentView.swift
+++ ios_tutorial_first_app/ContentView.swift
@@ -5,12 +5,17 @@ struct ContentView: View {
         VStack {
             Text("Swift！").padding().font(.headline)
             Text("Android！").padding().font(.largeTitle)
+            Button(action: {print("クロージャテスト")}) {
+                Text("ボタンだよ")
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
