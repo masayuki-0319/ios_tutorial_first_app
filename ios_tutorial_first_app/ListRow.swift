6cc5a4306faac4d066412ae2113cc41cb5ab2def
将之大西 <omccello@gmail.com>
2021-08-01 21:32:13 +0900

    fix

---
+++ ios_tutorial_first_app/ListRow.swift
@@ -0,0 +1,20 @@
+//
+//  ListRow.swift
+//  ios_tutorial_first_app
+//
+//  Created by 大西将之 on 2021/08/01.
+//
+
+import SwiftUI
+
+struct ListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ListRow()
+    }
+}
