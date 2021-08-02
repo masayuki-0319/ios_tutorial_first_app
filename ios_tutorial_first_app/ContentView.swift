09ba54f8e343e6e31d137eae4307c5089dde6720
将之大西 <omccello@gmail.com>
2021-08-03 00:52:23 +0900

    add

--- ios_tutorial_first_app/ContentView.swift
+++ ios_tutorial_first_app/ContentView.swift
@@ -1,12 +1,29 @@
 import SwiftUI
 
+struct Task: Identifiable {
+    let id = UUID()
+    var title: String
+    var checked: Bool
+    
+    init(title: String, checked: Bool) {
+        self.title = title
+        self.checked = checked
+    }
+}
+
 struct ContentView: View {
+    var tasks = [
+        Task(title: "Swift", checked: true),
+        Task(title: "Linux", checked: false),
+        Task(title: "Windows", checked: false)
+    ]
+    
     var body: some View {
         NavigationView {
             List {
-                ListRow(task: "Swift", isCheck: true)
-                ListRow(task: "Linux", isCheck: true)
-                ListRow(task: "Windows", isCheck: false)
+                ForEach(tasks) { task in
+                    ListRow(task: task.title, isCheck: task.checked)
+                }
                 Text ("+").font(.title)
             }
             .navigationTitle(Text("Tasks"))
